06df05ea3428972d458a3e0ea4b863ab9b356c92
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-10-14 17:23:04 +0800

    Add skeleton NoteClass

--- src/main/java/seedu/zettel/commands/Command.java
+++ src/main/java/seedu/zettel/commands/Command.java
@@ -7,7 +7,6 @@ import seedu.zettel.Storage;
 import java.util.ArrayList;
 
 public abstract class Command {
-    public abstract CommandResult execute(ArrayList<Note> notes, UI ui, Storage storage);
 
     public boolean isExit() {
         return false;
